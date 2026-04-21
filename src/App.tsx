ca923ac33431814dc09927b2bc03d7c6626e82e6
Nicolás Fishman <nicourifishman@gmail.com>
2026-04-21 18:06:58 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "malbec-ui";
+
+function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'lib/main.ts'),
+      name: 'malbec-ui',
+      // the proper extensions will be added
+      fileName: 'malbec-ui',
+    },
+    emptyOutDir: true,
+    rolldownOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    }
+  }
+})
